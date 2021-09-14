a180cbdc96111d0a6efbbdfa24207fc4ff29ba54
nwunderly <48489521+nwunderly@users.noreply.github.com>
2021-09-13 23:17:41 -0400

    update command file, wrangler config

--- worker/src/bot.ts
+++ worker/src/bot.ts
@@ -28,7 +28,7 @@ export async function handleRequest(request: Request): Promise<Response> {
   if (url === null) {
     return new Response('', { status: 404 })
   }
-  let response = await fetch(new Request(url))
+  let response = await fetch(new Request(`${url}/${interaction.data.name}.json`))
   let responseBody = await response.text()
 
   return respond(responseBody)
